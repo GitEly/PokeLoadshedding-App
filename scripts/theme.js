093bd86ef32cb11311eaa98b6ddb1a638f7a3048
Ely Boulot <e.weinum@gmail.com>
2023-01-26 10:53:49 +0200

    3 ways theme change added

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,10 +1,6 @@
-const themeSwitchers = document.getElementsByClassName("theme-box");
+let themeSheet = document.getElementById("theme-sheet");
 
-for (let i = 0; i < themeSwitchers.length; i++) {
-    const element = themeSwitchers[i];
-    element.addEventListener("click", changeTheme);
-}
-
-function changeTheme() {
-    console.log("that's working");
+function setTheme(selection) {
+    let newLink = "./styles/theme"+selection+".css";
+    themeSheet.setAttribute("href", newLink);
 }
