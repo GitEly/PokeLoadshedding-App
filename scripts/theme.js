df1bfea83d71943f5336dd6a9c1461d08d26199f
Ely Boulot <e.weinum@gmail.com>
2023-01-26 15:16:10 +0200

    styles imprvment

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,6 +1,6 @@
 let themeSheet = document.getElementById("theme-sheet");
 
 function setTheme(selection) {
-    let newLink = "./styles/theme"+selection+".css";
+    newLink = "./styles/theme"+selection+".css";
     themeSheet.setAttribute("href", newLink);
 }
