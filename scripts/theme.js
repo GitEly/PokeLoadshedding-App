8c727d119325ec634482e3405fa36f42afb8339f
Ely Weinum <e.weinum@gmail.com>
2023-01-29 20:01:34 +0200

    added gif files and cached theme feature

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,6 +1,25 @@
-let themeSheet = document.getElementById("theme-sheet");
 
-function setTheme(selection) {
+let themeSheet = document.getElementById("theme-sheet");
+let cachedTheme = localStorage.getItem('cachedTheme');
+
+function changeTheme(selection) {
     newLink = "./styles/theme"+selection+".css";
     themeSheet.setAttribute("href", newLink);
+    localStorage.setItem('cachedTheme', selection);
 }
+
+function setTheme(theme) {
+    switch (theme) {
+        case 'Charmander':
+            changeTheme(theme);
+            break;
+        case 'Squirtle':
+            changeTheme(theme);
+            break;
+        default:
+            changeTheme('Pikachu');
+            break;
+    }
+}
+
+window.onload = setTheme(cachedTheme);
