fbf6ed1ebf6d6d5f6a8cb4f81127ea7c3ad03ee8
Ely Weinum <e.weinum@gmail.com>
2023-02-11 10:25:00 +0200

    updates from last job

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,9 +1,10 @@
 
 let themeSheet = document.getElementById("theme-sheet");
 let cachedTheme = localStorage.getItem('cachedTheme');
+var currentTheme;
 
 // function changeTheme 
-// is called when user press a theme button, change theme css url
+// is called when user press a theme button, change theme css url accordingly
 // store selection to local storage
 
 
@@ -13,16 +14,23 @@ function changeTheme(selection) {
     localStorage.setItem('cachedTheme', selection);
 }
 
+// function setTheme 
+// is called onload
+// set theme to local storage value or pika by default
+
 function setTheme(theme) {
     switch (theme) {
         case 'Charmander':
             changeTheme(theme);
+            currentTheme = 'Charmander';
             break;
         case 'Squirtle':
             changeTheme(theme);
+            currentTheme = 'Squirtle';
             break;
         default:
             changeTheme('Pikachu');
+            currentTheme = "Pikachu"
             break;
     }
 }
