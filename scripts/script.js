e047753756ba69b8710d3735c41337bc8b48eee9
Ely Weinum <e.weinum@gmail.com>
2023-02-20 20:30:19 +0200

    merged theme js and script js, change thm and html

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,57 @@
-// let isLs = false;
-// let lsStatus;
-// let currentLsTimes = document.getElementById('current-data-time');
-// let lsLogo = document.getElementById('status-emoji').innerHTML;
-// let powerStatus = document.getElementById('status-power').innerHTML;
-// lsStatus = isLs == true ? lsStatus = "Down" : "Up";
 
-// if (isLs == true) {
-//     lsLogo = "🚫";
-//     powerStatus = "OFF";
-//     currentLsTimes.style.display = "inline";
-// }else if(isLs == false){
-//     lsLogo = "✅";
-//     powerStatus = "ON";
-//     currentLsTimes.style.display = "none";
-// }
+isLS = true;
 
+// this script manage theme changes, storing into cache and
+// retrieve if there is a cached theme
 
-console.log(currentTheme);
-let isLS = true;
+let cachedTheme = localStorage.getItem('cachedTheme');
+let themeCss = document.getElementById('theme-sheet');
+let currentTheme = "Pikachu";
 
+const storeTheme = (theme) => localStorage.setItem('cachedTheme', theme);
 
-if (isLs == true){
-    logo = "🚫";
-    gif = "./ressources/img/"+theme+"Sad.gif"
-}else{
-    logo = "✅";
-    gif = "./ressources/img/"+theme+"Happy.gif"
+const setTheme = (theme) => {
+    currentTheme = theme;
+    cssPath = "./styles/theme"+theme+".css";
+    themeCss.setAttribute("href", cssPath);
+    storeTheme(theme);
 }
+
+const checkTheme = () => {
+    if (cachedTheme) {
+    setTheme(cachedTheme);
+    currentTheme = cachedTheme;
+}else{
+    setTheme("Pikachu");
+}
+}
+checkTheme();
+
+// block bellow handle content html according LS status and current theme
+
+function lsDisplay(lsLogo,lStatus,lsGif,lsTimesDisplay){
+    document.getElementById("status-emoji").innerHTML = lsLogo;
+    document.getElementById("illustrationGif").src = lsGif;
+    document.getElementById("status-power").innerHTML = lStatus;
+    document.getElementById('current-data-time').style.display = lsTimesDisplay;
+}
+
+function updateHTMLcontent(){
+    if(isLS) {
+        lsDisplay("🚫","OFF","./ressources/img/"+currentTheme+"Sad.gif","inline")
+    }else if(isLS == null){
+        lsDisplay("-","🤔","./ressources/img/NoData.gif","none")
+    }else{
+        lsDisplay("✅","ON","./ressources/img/"+currentTheme+"Happy.gif","none")
+    }
+};
+
+updateHTMLcontent()
+
+const clickChangeTheme = (theme) => {
+    setTheme(theme);
+    updateHTMLcontent()
+}
+
+console.log(isLS);
+console.log(currentTheme);
